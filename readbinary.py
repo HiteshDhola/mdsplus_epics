7c2ade7511514da6b89c56c251b5719744ea93ca
hitesh <hitesh@0e8baa46-8608-0149-a96a-3787c3d3c516>
2020-03-12 15:18:33 +0000

    git-svn-id: https://hdhola/svn/codas/mdsplus/trunk@31 0e8baa46-8608-0149-a96a-3787c3d3c516

--- readbinary.py
+++ readbinary.py
@@ -1,5 +1,6 @@
 import numpy
 import time
+import datetime
 
 def to_seconds(h):
    return (h>>32) + ((float)(h&0xffffffff))/pow(2,32)
@@ -19,6 +20,15 @@ time_stamp= array[5][0]
 time_conv=(float(time_stamp)/1e7)
 print(time_conv)
 print("\n Time Stamp (64 bit) ","%d" %time_stamp)
-print("\n Shot taken on", time.ctime(to_seconds(3630550620525219330)))
+print("\n Shot taken on", time.ctime(to_seconds(1460000000100000000)))
+
+#3614846037-‭2082911364‬=1531934673 (1904-1970) #2082844800
+
+from datetime import datetime
+
+
+time2=datetime.now().timestamp()
+print(datetime.now())
+print(time2)
 
 f.close()
